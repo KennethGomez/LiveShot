83a884b61339468828fb4277b7219df7bd2f7dd5
KennethGómez <kennethgomezfdz@gmail.com>
2021-12-29 14:20:44 +0100

    Implement keyboard hook

--- LiveShot.API/Background/BackgroundApplication.cs
+++ LiveShot.API/Background/BackgroundApplication.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using LiveShot.API.Background.ContextOptions;
 using LiveShot.API.Properties;
@@ -9,6 +11,19 @@ namespace LiveShot.API.Background
 {
     public class BackgroundApplication : IBackgroundApplication
     {
+        private delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr _hHook;
+        
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowsHookEx", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+        
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
         private readonly IEnumerable<IContextOption> _contextOptions;
         private readonly IContextOption _captureScreenShotOption;
 
@@ -20,6 +35,8 @@ namespace LiveShot.API.Background
 
         public void Init()
         {
+            CaptureKeyboardKeys();
+
             var notifyIcon = new NotifyIcon();
 
             notifyIcon.Icon = new System.Drawing.Icon("bg-icon.ico");
@@ -36,6 +53,31 @@ namespace LiveShot.API.Background
             }
 
             notifyIcon.ContextMenuStrip = menuStripOptions;
+        }
+
+        private void CaptureKeyboardKeys()
+        {
+            _hHook = SetWindowsHookEx(
+                WH_KEYBOARD_LL,
+                OnKeyBoardMessage,
+                Marshal.GetHINSTANCE(typeof(BackgroundApplication).Module),
+                0
+            );
+        }
+
+        private int OnKeyBoardMessage(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int keycode = Marshal.ReadInt32(lParam);
+
+                if (wParam == (IntPtr)WM_KEYDOWN)
+                {
+                    Debug.WriteLine(keycode);
+                }
+            }
+
+            return CallNextHookEx(_hHook, nCode, wParam, lParam);
         }
     }
 }
