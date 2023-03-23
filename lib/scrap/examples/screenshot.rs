bc282599204ab0813262a382e70738e0efcee022
Kenneth Gómez <kenneth.gomez@saalg.com>
2023-03-23 13:11:24 +0100

    add: `scrap` missing files

---
+++ lib/scrap/examples/screenshot.rs
@@ -0,0 +1,65 @@
+extern crate repng;
+extern crate scrap;
+
+use scrap::{Capturer, Display};
+use std::io::ErrorKind::WouldBlock;
+use std::fs::File;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let one_second = Duration::new(1, 0);
+    let one_frame = one_second / 60;
+
+    let display = Display::primary().expect("Couldn't find primary display.");
+    let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
+    let (w, h) = (capturer.width(), capturer.height());
+
+    loop {
+        // Wait until there's a frame.
+
+        let buffer = match capturer.frame() {
+            Ok(buffer) => buffer,
+            Err(error) => {
+                if error.kind() == WouldBlock {
+                    // Keep spinning.
+                    thread::sleep(one_frame);
+                    continue;
+                } else {
+                    panic!("Error: {}", error);
+                }
+            }
+        };
+
+        println!("Captured! Saving...");
+
+        // Flip the ARGB image into a BGRA image.
+
+        let mut bitflipped = Vec::with_capacity(w * h * 4);
+        let stride = buffer.len() / h;
+
+        for y in 0..h {
+            for x in 0..w {
+                let i = stride * y + 4 * x;
+                bitflipped.extend_from_slice(&[
+                    buffer[i + 2],
+                    buffer[i + 1],
+                    buffer[i],
+                    255,
+                ]);
+            }
+        }
+
+        // Save the image.
+
+        repng::encode(
+            File::create("screenshot.png").unwrap(),
+            w as u32,
+            h as u32,
+            &bitflipped,
+        ).unwrap();
+
+        println!("Image saved to `screenshot.png`.");
+        break;
+    }
+}
---
+++ lib/scrap/src/x11/display.rs
@@ -0,0 +1,54 @@
+use super::ffi::*;
+use super::Server;
+use std::rc::Rc;
+
+#[derive(Debug)]
+pub struct Display {
+    server: Rc<Server>,
+    default: bool,
+    rect: Rect,
+    root: xcb_window_t,
+    name: xcb_atom_t,
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+pub struct Rect {
+    pub x: i16,
+    pub y: i16,
+    pub w: u16,
+    pub h: u16,
+}
+
+impl Display {
+    pub unsafe fn new(
+        server: Rc<Server>,
+        default: bool,
+        rect: Rect,
+        root: xcb_window_t,
+        name: xcb_atom_t,
+    ) -> Display {
+        Display {
+            server,
+            default,
+            rect,
+            root,
+            name,
+        }
+    }
+
+    pub fn server(&self) -> &Rc<Server> {
+        &self.server
+    }
+    pub fn is_default(&self) -> bool {
+        self.default
+    }
+    pub fn rect(&self) -> Rect {
+        self.rect
+    }
+    pub fn root(&self) -> xcb_window_t {
+        self.root
+    }
+    pub fn name(&self) -> xcb_atom_t {
+        self.name
+    }
+}
